c2c54ae06f3c7f1995fe9527c97c84a9a2293c51
Dis <sr_5@inbox.ru>
2013-11-15 18:16:27 +0700

    initial commit:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = '1fa9d5635cdf5c9a00052a14fa7ebd4718142a7636c5360db361de7defbfb28357b71170f59f6603dab3e0395187d6f9a84da60ed88522039a5da0ddb05731e7'
